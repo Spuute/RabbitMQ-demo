301e9cab10bc2b87ba41f944517a5403243f2bc8
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2022-05-08 16:14:55 +0200

    Initial

---
+++ source/Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+List<Person> persons = new List<Person>();
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox", 
+    durable: false, 
+    exclusive: false, 
+    autoDelete: false, 
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) => {
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var deSerialized = JsonSerializer.Deserialize<Person>(message);
+
+    System.Console.WriteLine($"Person recieved {deSerialized}");
+    persons.Add(deSerialized);
+};
+
+channel.BasicConsume(queue: "letterbox", autoAck: true, consumer: consumer);
+
+
+Console.ReadKey();
+
+
+public class Person {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+
+};
---
+++ source/Producer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox", 
+    durable: false, 
+    exclusive: false, 
+    autoDelete: false, 
+    arguments: null);
+
+Person person = new Person() { 
+    Id = 3,
+    Name = "Kevin Kallin",
+    Address = "Göteborgsvägen 130"
+};
+
+// Serialize object to JSON
+string serialize = JsonSerializer.Serialize(person);
+
+
+// Encode Json to byte[]
+var encodedMessage = Encoding.UTF8.GetBytes(serialize);
+
+channel.BasicPublish("", "letterbox", null, encodedMessage);
+
+System.Console.WriteLine($"Published message: {serialize}");
+
+
+public class Person {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+};
